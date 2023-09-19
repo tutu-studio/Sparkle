a53d137cc5bd83e433e656f1badc71ec71766011
Tualatrix Chou <tualatrix@gmail.com>
2023-09-19 16:56:32 +0800

    Update Package.swift for new path and checksum

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 let version = "2.5.0"
 // Tag is required to point towards the right asset. SPM requires the tag to follow semantic versioning to be able to resolve it.
 let tag = "2.5.0"
-let checksum = "f7e6ef91edee5704b76358c9485edc63a79dfefe770d8b5340818b7db69187df"
-let url = "https://github.com/sparkle-project/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
+let checksum = "2a71a7f62f013e9fa9f551067a1d315a8c76af45bd392ab0401f82177b1b1508"
+let url = "https://github.com/tutu-studio/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
 
 let package = Package(
     name: "Sparkle",
