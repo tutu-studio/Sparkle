7954ded2ec58dbfcce3b05de9cb578677f4ca8fa
Tualatrix Chou <tualatrix@gmail.com>
2024-07-01 19:29:10 +0800

    Update for self-build version 2.6.4

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 let version = "2.6.4"
 // Tag is required to point towards the right asset. SPM requires the tag to follow semantic versioning to be able to resolve it.
 let tag = "2.6.4"
-let checksum = "eb3814726816ca2f09334ce562fff1b2cd0f7c16dd4e283256081f307accb9c4"
-let url = "https://github.com/sparkle-project/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
+let checksum = "2a1c8223c363c39611b72ae397c383c197439d64e490d069ed0767cd7f2a75a7"
+let url = "https://github.com/tutu-studio/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
 
 let package = Package(
     name: "Sparkle",
